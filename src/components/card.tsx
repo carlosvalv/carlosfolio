cf7df560beda407b48675a8f91e5e4a9e206cfb7
Carlos Valverde <carlos.valverde@tusmedia.com>
2023-04-22 17:15:40 +0200

    [Card] card component

---
+++ src/components/card.tsx
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 200px;
+  min-width: 200px;
+  min-height: 260px;
+  padding: 14px;
+  border-radius: 16px;
+  background-color: #2F2F2F;
+  gap: 1em;
+  display: flex;
+  flex-direction: column;
+  margin: auto;
+`;
+
+const TitleCard = styled.h4`
+  color: #fff;
+  text-align: center;
+  margin: 0;
+  font-weight: 500;
+  border-bottom: 1px solid;
+  padding-bottom: 12px;
+`;
+
+const Tags = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-flow: row wrap;
+  gap: 8px;
+`;
+
+const Tag = styled.span`
+  background-color: #fff;
+  padding: 5px 10px;
+  border-radius: 25px;
+  font-size: 14px;
+  font-weight: 600;
+`;
+
+const Desc = styled.p`
+  color: #fff;
+  font-size: 1.25vh;
+`;
+
+const Footer = styled.div`
+  background-color: #fff;
+  border-radius: 14px;
+  text-align: center;
+  margin-top: auto;
+`;
+
+const Link = styled.a`
+  color: #2F2F2F;
+  text-decoration: none;
+  width: calc(100% - 12px);
+  display: block;
+  padding: 6px;
+`;
+
+type CardProps = {
+  title: string,
+  tags: string[],
+  desc: string,
+  url: string
+}
+
+export function Card(props: CardProps) {
+  return (
+    <Container>
+      <TitleCard>{props.title}</TitleCard>
+      <Tags>
+        {props.tags.map((tag : string) =>{
+          return <Tag>{tag}</Tag>
+        })}
+      </Tags>
+      <Desc>{props.desc}</Desc>
+      <Footer>
+        <Link target='_blank' rel={"noreferrer"} href={props.url}>View Project</Link>
+      </Footer>
+    </Container>
+  );
+}
